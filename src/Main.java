57fae08cf1a055d9a66b0950e6e744efd366a652
danilogms <Danmil-20@hotmail.com>
2024-11-08 14:35:39 -0300

    add Class Curso, add Class Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo(" == Curso Java Avançado == ");
+        curso.setDescricao("Se trata de um curso avançado para programadores de JAVA!");
+        curso.setCargaHoraria(10);
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo(" == Mentoria em SQL == ");
+        mentoria.setDescricao("Se trata de uma mentoria para desenvolvedores Backend.");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
     }
 }
